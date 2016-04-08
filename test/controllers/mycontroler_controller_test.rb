6545a0f2098f052150e0a6c03069b17d70d49f56
Arash Rehaman <mafzhussain@gmail.com>
2016-04-08 18:41:28 +0000

    New test file added

--- test/controllers/mycontroler_controller_test.rb
+++ test/controllers/mycontroler_controller_test.rb
@@ -1,14 +1,5 @@
 require 'test_helper'
 
 class MycontrolerControllerTest < ActionController::TestCase
-  test "should get Home" do
-    get :Home
-    assert_response :success
-  end
-
-  test "should get Show" do
-    get :Show
-    assert_response :success
-  end
 
 end
